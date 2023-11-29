c98cffdc63c20d2643ad2a6ccd6b4dfa9f2dae99
Nguyen Hong Phuc <PhucNHb@bkav.com>
2023-11-29 15:51:46 +0700

    Sửa lại màn NoMath cảnh báo đường dẫn không hợp lệ

--- src/screens/NoMatch/index.tsx
+++ src/screens/NoMatch/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Button, Result } from 'antd';
+import {useNavigate} from "react-router-dom";
 
 function NoMatch() {
+    const navigate = useNavigate();
     return (
-        <div style={{ padding: 20 }}>
-            <h2>Không tìm thấy trang</h2>
-        </div>
+        <Result
+            status="404"
+            title="SOS"
+            subTitle="Xin lỗi, không tồn tại đường dẫn trên"
+            extra={<Button type="primary" onClick={() => navigate('/')}>Trang chủ</Button>}
+        />
     );
 }
 
